87fbc994340ca992b363a3684d91212a11585d66
GOMEZ PEREZ,LUIS RODRIGO <lugomez@externos.santander.com.mx>
2021-07-13 14:29:26 -0500

    proyect upload

--- App.js
+++ App.js
@@ -1,12 +1,80 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+{/* Read me tutorial video https://www.youtube.com/watch?v=00HFzh3w1B8&list=PLYBvEAka-q1hJuwRPYQPlEBBRm7_qGw_2
+source code: https://github.com/mattfrances/simpleReactNativeTodoList  */}
+
+
+import Task from './components/task';
+import React, {useState} from 'react';
+import { Platform,
+         StyleSheet,
+         Text,
+         View,
+         KeyboardAvoidingView,
+         TextInput,
+         TouchableOpacity, 
+         Keyboard,
+         ScrollView} from 'react-native';
 
 export default function App() {
+
+  const [task, setTask] = useState();
+  const [taskItems,setTaskItems] = useState([]);
+
+  const handleAddTask = () => {
+    setTaskItems([...taskItems, task ]);
+    setTask("");
+    Keyboard.dismiss();
+    console.log(task);
+  }
+
+  const completeTask = (index) => {
+    let itemsCopy = [...taskItems];
+    itemsCopy.splice(index, 1);
+    setTaskItems(itemsCopy);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+      {/* Added this scroll view to enable scrolling when list gets longer than the page */}
+      <ScrollView
+        contentContainerStyle={{
+          flexGrow: 1
+        }}
+        keyboardShouldPersistTaps='handled'
+      >
+
+        {/* Todays Tasks  seccion*/}
+        <View style={styles.tasksWrapper}>
+          <Text style={styles.sectionTitle}>RodAsh - Today´s Tasks</Text>
+          <View style={styles.items}>
+            {/* Tasks list seccion*/}
+            {
+              taskItems.map((items, index) => {
+                return (
+                  <TouchableOpacity key={index} onPress={() => completeTask(index)}>
+                    <Task text={items}/>
+                  </TouchableOpacity>
+                ) 
+              })
+            }
+          </View>
+        </View>
+      </ScrollView>
+
+      {/* Write a Task seccion*/}
+      <KeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+        style={styles.writeTextWrapper}
+      >
+        <TextInput style={styles.input} placeholder={'Write a Task'} value={task} onChangeText={text => setTask(text)} />
+        <TouchableOpacity onPress={() => handleAddTask()}>
+          <View style={styles.addWrapper}>
+            <Text style={styles.addText}>+</Text>
+          </View>
+        </TouchableOpacity>
+
+      </KeyboardAvoidingView>
+        
     </View>
   );
 }
@@ -14,8 +82,49 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#E8EAED',
   },
+  tasksWrapper: {
+    paddingTop: 80,
+    paddingHorizontal: 20,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  items: {
+    marginTop: 30,
+  },
+  writeTextWrapper: {
+    position: 'absolute',
+    bottom: 60,
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+
+  },
+  input: {
+    paddingVertical: 15,
+    width: 250,
+    paddingHorizontal: 15,
+    backgroundColor: "#FFF",
+    borderRadius: 60,
+    borderColor: "#C0C0C0",
+    borderWidth: 1,
+  },
+  addWrapper: {
+    width: 60,
+    height: 60,
+    backgroundColor: "#FFF",
+    borderRadius: 60,
+    justifyContent: 'center',
+    alignItems:'center',
+    borderColor: "#C0C0C0",
+    borderWidth: 1,
+
+  },
+  addText: {
+    
+  },  
 });
